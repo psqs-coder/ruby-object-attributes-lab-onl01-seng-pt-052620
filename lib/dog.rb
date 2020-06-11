31a40123db9b964547f817b1003edc5a54129983
Bruce Parker <detailsharks@gmail.com>
2020-06-11 02:07:09 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+class Dogs_name
+  def intitialize (name) 
+    @name= name 
+  end
+
+def name 
+  @name
+end
+
+
+
+
+
+def intitialize (snoopy.breed)
+  snoopy.breed= breed
+end
+
+def snoopy.breed 
+  @snoopy.breed
+end
+
+
+
+
+
+
+
+
