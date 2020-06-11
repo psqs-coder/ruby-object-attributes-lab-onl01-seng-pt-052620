f4a09c846ffe3999e8e5f3f68f29237c58f66f05
Bruce Parker <detailsharks@gmail.com>
2020-06-11 02:27:42 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,25 +1,5 @@
 class Person
   
-  def name = (name)
-    @name = name
-  end
-  
-  def name
-    @name
-  end
-end
-
-def job = (job)
-  @job = job
-end
-
-def job 
-  @job
-end
-end
-  
-  class Person
-  
   def name=(name)
     @name = name
   end
@@ -27,12 +7,15 @@ end
   def name
     @name
   end
+end
+def job=(job)
+  @job = job
+end
 
-  def job=(job)
-    @job = job
-  end 
-
-  def job 
-    @job
-  end 
+def job
+  @job
+end
+end
+  
+  
   
